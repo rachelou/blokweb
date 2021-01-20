cf8dea75e6ea3536a11864b0ac6aea24dc3cb28b
rachelou <74224093+rachelou@users.noreply.github.com>
2021-01-20 13:00:34 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,21 @@
+document.querySelector(".container").addEventListener("click", myFunction);
 
-function myFunction(open) {
-  open.classList.toggle("change");
-  document.getElementsByClassName("nav-items").style.visibility = "hidden";
+function myFunction() {
+  var element = document.querySelector(".nav-items");
+  var products = document.querySelector (".products");
+  var hero = document.querySelector (".hero");
+  var second = document.querySelector (".second");
+  var article = document.querySelector ("article")
+
+  document.body.classList.toggle('change');
+  if(element.style.display === "block"){
+          element.style.display = "none";
+          document.body.style.overflow = 'auto';
+
+      }else{
+          element.style.display = "block";
+            document.body.style.overflow = 'hidden';
+      }
+
+
 }
